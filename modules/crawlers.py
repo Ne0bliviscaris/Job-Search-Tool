bffbe3a8fb605220ff382adbca7ae4461d7bd922
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-08-26 20:21:09 +0200

    JobRecord class now handles record processing

--- modules/crawlers.py
+++ modules/crawlers.py
@@ -1,15 +1,6 @@
-from containers import set_record_containers, set_search_containers
-from scrappers.listing_scrappers import (
-    detect_records,
-    fetch_company_logo,
-    fetch_company_name,
-    fetch_job_location,
-    fetch_job_tags,
-    fetch_job_title,
-    fetch_job_url,
-    fetch_salary_range,
-    get_search_block,
-)
+from containers import set_search_containers
+from JobRecord import JobRecord
+from scrappers.listing_scrappers import detect_records, get_search_block
 from websites import identify_website, search_links
 
 
@@ -34,36 +25,12 @@ def search_site(search_link):
     search_block = get_search_block(search_link, search_container)
     search_records = detect_records(search_block, record_container)
 
-    # Przetwarzanie każdego rekordu w search_records
-    extracted_record = [extract_from_record(current_website, record) for record in search_records]
+    # Process HTML code into JobRecord objects
+    extracted_record = [JobRecord(record, current_website) for record in search_records]
     return extracted_record
     # return search_results
 
 
-def extract_from_record(current_website, search_records):
-    """
-    Extract job title, tags, salary, location, URL and company name from the record
-    """
-    title_container, tags_container, company_container, logo_container, location_container, salary_container = (
-        set_record_containers(current_website)
-    )
-    job_url = fetch_job_url(search_records)
-    job_title = fetch_job_title(search_records, title_container)
-    job_tags = fetch_job_tags(search_records, tags_container)
-    company_name = fetch_company_name(search_records, company_container)
-    logo = fetch_company_logo(search_records, logo_container)
-    location = fetch_job_location(search_records, location_container)
-    salary = fetch_salary_range(search_records, salary_container)
-    return job_title, job_url, job_tags, company_name, logo, location, salary
-
-
-# Test the code
-# nfj = search_links[0]
-# job_listing = search_site(nfj)[1]
-# print(job_listing)
-
-# Test full search
+# Test full search and return the first record
 full_search = search_all_sites()
-import pprint
-
-pprint.pprint(full_search)
+print(full_search[0][0])
