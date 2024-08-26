c0fa53f78fb33786ec2e266c405772957c77ee7e
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-08-26 18:18:29 +0200

    search links and site identification

--- modules/websites.py
+++ modules/websites.py
@@ -6,7 +6,8 @@ ROCKETJOBS = "rocketjobs"
 current_website = None
 
 search_links = [
-    "https://nofluffjobs.com/pl/artificial-intelligence?criteria=category%3Ddata%20seniority%3Dtrainee,junior"
+    # NFJ Data AI Trainee Junior
+    "https://nofluffjobs.com/pl/artificial-intelligence?criteria=category%3Ddata%20seniority%3Dtrainee,junior",
 ]
 
 
@@ -14,7 +15,6 @@ def identify_website(search_link):
     """
     Set current website as global variable based on search link
     """
-    # print(f"Link received: {search_link}")
     global current_website
     if "nofluffjobs.com" in search_link:
         current_website = NOFLUFFJOBS
@@ -26,21 +26,5 @@ def identify_website(search_link):
         current_website = ROCKETJOBS
     else:
         current_website = "Error: Website not recognized"
-    # print(f"Current website: {current_website}")
+
     return current_website
-
-
-# def set_site_parameters(site, job):
-
-#     # NoFluffJobs
-#     if site == "nofluffjobs":
-#         search_container = "nfj-postings-list"
-#         job_name = job.find(attrs={"data-cy": "title position on the job offer listing"}).text
-#         job_tags = [tag.text for tag in job.find_all(attrs={"data-cy": "category name on the job offer listing"})]
-#         salary_elements = job.find(attrs={"data-cy": "salary ranges on the job offer listing"})
-#         job_location = [
-#             loc.text.strip() for loc in job.find_all(attrs={"data-cy": "location on the job offer listing"})
-#         ]
-#         job_url = "https://nofluffjobs.com" + job["href"]
-#         company_name = job.find("h4").text.strip()
-#     return search_container, job_name, job_tags, salary_elements, job_location, job_url, company_name
