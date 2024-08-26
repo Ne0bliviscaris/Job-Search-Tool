d0981f9768cf829a8742f8dbd35d3a5230092456
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-08-26 20:20:09 +0200

    separate function for each container

--- modules/containers.py
+++ modules/containers.py
@@ -24,29 +24,109 @@ def set_search_containers(current_website):
     return search_container, record_container
 
 
-# Backup - containers within records
-def set_record_containers(current_website):
+def job_title(current_website):
     """
-    Zwraca wartości kontenerów dla każdej strony internetowej
+    Returns title container for each website
     """
     if current_website == NOFLUFFJOBS:
         title_container = {"data-cy": "title position on the job offer listing"}
+
+    elif current_website == JUSTJOIN:
+        title_container = "TO BE DONE --------------"
+
+    elif current_website == ROCKETJOBS:
+        title_container = "TO BE DONE --------------"
+
+    elif current_website == THEPROTOCOL:
+        title_container = "TO BE DONE --------------"
+    return title_container
+
+
+def tags(current_website):
+    """
+    Returns tags container for each website
+    """
+    if current_website == NOFLUFFJOBS:
         tags_container = {"data-cy": "category name on the job offer listing"}
-        company_container = "h4"
-        logo_container = "img"
+
+    elif current_website == JUSTJOIN:
+        tags_container = "TO BE DONE --------------"
+
+    elif current_website == ROCKETJOBS:
+        tags_container = "TO BE DONE --------------"
+
+    elif current_website == THEPROTOCOL:
+        tags_container = "TO BE DONE --------------"
+    return tags_container
+
+
+def company(current_website):
+    """
+    Returns company name container for each website
+    """
+    if current_website == NOFLUFFJOBS:
+        company_name = "h4"
+
+    elif current_website == JUSTJOIN:
+        company_name = "TO BE DONE --------------"
+
+    elif current_website == ROCKETJOBS:
+        company_name = "TO BE DONE --------------"
+
+    elif current_website == THEPROTOCOL:
+        company_name = "TO BE DONE --------------"
+    return company_name
+
+
+def logo(current_website):
+    """
+    Returns logo container for each website
+    """
+    if current_website == NOFLUFFJOBS:
+        logo_container = {"alt": "Company logo"}
+
+    elif current_website == JUSTJOIN:
+        logo_container = "TO BE DONE --------------"
+
+    elif current_website == ROCKETJOBS:
+        logo_container = "TO BE DONE --------------"
+
+    elif current_website == THEPROTOCOL:
+        logo_container = "TO BE DONE --------------"
+    return logo_container
+
+
+def location(current_website):
+    """
+    Returns location container for each website
+    """
+    if current_website == NOFLUFFJOBS:
         location_container = {"data-cy": "location on the job offer listing"}
+
+    elif current_website == JUSTJOIN:
+        location_container = "TO BE DONE --------------"
+
+    elif current_website == ROCKETJOBS:
+        location_container = "TO BE DONE --------------"
+
+    elif current_website == THEPROTOCOL:
+        location_container = "TO BE DONE --------------"
+    return location_container
+
+
+def salary(current_website):
+    """
+    Returns salary container for each website
+    """
+    if current_website == NOFLUFFJOBS:
         salary_container = {"data-cy": "salary ranges on the job offer listing"}
 
     elif current_website == JUSTJOIN:
-        search_container = "TO BE DONE --------------"
-        record_container = "TO BE DONE --------------"
+        salary_container = "TO BE DONE --------------"
 
     elif current_website == ROCKETJOBS:
-        search_container = "TO BE DONE --------------"
-        record_container = "TO BE DONE --------------"
+        salary_container = "TO BE DONE --------------"
 
     elif current_website == THEPROTOCOL:
-        search_container = "TO BE DONE --------------"
-        record_container = "TO BE DONE --------------"
-
-    return title_container, tags_container, company_container, logo_container, location_container, salary_container
+        salary_container = "TO BE DONE --------------"
+    return salary_container
