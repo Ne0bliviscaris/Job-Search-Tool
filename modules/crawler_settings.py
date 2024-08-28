b12e86628f4da48083785099e488dc5cc8a70d42
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-08-28 18:51:11 +0200

    clean up obsolete file

--- modules/crawler_settings.py
+++
@@ -1,7 +0,0 @@
-# NoFluffJobs
-    job_name = job.find(attrs={"data-cy": "title position on the job offer listing"}).text
-    job_tags = [tag.text for tag in job.find_all(attrs={"data-cy": "category name on the job offer listing"})]
-    salary_elements = job.find(attrs={"data-cy": "salary ranges on the job offer listing"})
-    job_location = [loc.text.strip() for loc in job.find_all(attrs={"data-cy": "location on the job offer listing"})]
-    job_url = "https://nofluffjobs.com" + job["href"]
-    company_name = job.find("h4").text.strip()
