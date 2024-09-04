bcb3c3b4a6b27dbdecd76d4c994d3eea0b053c00
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-09-04 14:27:20 +0200

    naming improvements

--- modules/data_collector.py
+++ modules/data_collector.py
@@ -4,11 +4,11 @@ from data_processor import build_dataframe, html_to_soup, process_records
 from websites import search_links
 
 
-def set_filename(link):
+def set_filename(search_link):
     """
     Generate a readable filename based on the combined link
     """
-    return os.path.join("modules/sites", f"{link}.html")
+    return os.path.join("modules/sites", f"{search_link}.html")
 
 
 def search_all_sites():
@@ -19,24 +19,24 @@ def search_all_sites():
     all_search_results = []
     for link, search_link in search_links.items():
         if PRINTS:
-            print(f"[data_collector.py - search_all_sites] Searching site with link: {link}")
+            print(f"[data_collector.py - search_all_sites] Searching site with link: {search_link}")
         search_result = search_site(link)
         all_search_results.append(search_result)
     return all_search_results
 
 
-def search_site(link):
+def search_site(search_link):
     """
     Get HTML block containing job search results from a file
     """
     PRINTS = False
-    filename = set_filename(link)
+    filename = set_filename(search_link)
     soup = html_to_soup(filename)
     if soup is None:
         if PRINTS:
             print(f"[data_collector.py - search_site] File not found: {filename}")
         return []
-    return process_records(soup, link)
+    return process_records(soup, search_link)
 
 
 if __name__ == "__main__":
