84511f8048e51f22e98641dd53ed01ca1295aed8
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-09-04 18:24:13 +0200

    added theprotcol link

--- modules/websites.py
+++ modules/websites.py
@@ -8,6 +8,7 @@ current_website = None
 search_links = {
     "nofluffjobs_data-ai-trainee-junior": "https://nofluffjobs.com/pl/artificial-intelligence?criteria=category%3Ddata%20seniority%3Dtrainee,junior",
     "nofluffjobs_data-ai-trainee-junior-mid": "https://nofluffjobs.com/pl/artificial-intelligence?criteria=category%3Ddata%20seniority%3Dtrainee,junior,mid",
+    "theprotocol_big-data-ai-ml-junior-assistant-trainee": "https://theprotocol.it/filtry/big-data-science,ai-ml;sp/junior,assistant,trainee;p",
 }
 
 
