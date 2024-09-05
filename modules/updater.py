83aa9c172e59f68e9f59d330a8c3a2b8bc15126e
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-09-05 16:45:37 +0200

    major cleanup, using only selenium now

--- modules/updater.py
+++ modules/updater.py
@@ -1,53 +1,21 @@
 import os
 
 import containers as containers
-import requests
-from bs4 import BeautifulSoup
 from data_collector import set_filename
 from selenium_utils import get_container, setup_webdriver
 from websites import identify_website, search_links
 
 
-def fetch_html_requests(url: str) -> str:
-    """
-    Fetch HTML content from a URL using requests
-    """
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.content
-
-
-def fetch_html_selenium(url: str, search_container: str) -> str:
+def scrape(url: str, search_container: str) -> str:
     """
     Fetch HTML content from a URL using Selenium
     """
     driver = setup_webdriver()
     with driver:
         driver.get(url)
-        driver.implicitly_wait(5)
-        search_block_html = get_container(driver, search_container)
-    return search_block_html
-
-
-def parse_with_beautifulsoup(html_content: str, search_container: dict) -> BeautifulSoup:
-    """
-    Parse HTML content using BeautifulSoup
-    """
-    soup = BeautifulSoup(html_content, "html.parser")
-    return soup.find(search_container)
-
-
-def scrape(search_link: str, search_container: dict) -> BeautifulSoup:
-    """
-    Get HTML block containing job search results
-    """
-    USE_SELENIUM = True
-    if USE_SELENIUM:
-        html_content = fetch_html_selenium(search_link, search_container)
-    else:
-        html_content = fetch_html_requests(search_link)
-    job_listing = parse_with_beautifulsoup(html_content, search_container)
-    return job_listing
+        driver.implicitly_wait(7)
+        html_content = get_container(driver, search_container)
+    return html_content
 
 
 def get_search_container(link: str) -> dict:
