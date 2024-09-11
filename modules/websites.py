c72b84c7c0e61f7a620a35e1941e1168f1ef98cd
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-09-11 12:51:24 +0200

    all sites active

--- modules/websites.py
+++ modules/websites.py
@@ -2,7 +2,7 @@ NOFLUFFJOBS = "https://nofluffjobs.com"
 THEPROTOCOL = "https://theprotocol.it"
 BULLDOGJOB = "https://bulldogjob.pl"
 ROCKETJOBS = "https://rocketjobs.pl"
-
+JUSTJOINIT = "https://justjoin.it"
 current_website = None
 
 search_links = {
@@ -10,6 +10,7 @@ search_links = {
     "theprotocol_big-data-ai-ml-junior-assistant-trainee": "https://theprotocol.it/filtry/big-data-science,ai-ml;sp/junior,assistant,trainee;p",
     "bulldogjob_remote-data": "https://bulldogjob.pl/companies/jobs/s/locationDistance,50/role,data/city,Remote",
     "rocketjobs_remote-data": "https://rocketjobs.pl/wszystkie-lokalizacje/bi-data/praca-zdalna_tak",
+    "justjoinit_data-junior-remote": "https://justjoin.it/all-locations/data/experience-level_junior/remote_yes?orderBy=DESC&sortBy=newest",
 }
 
 
@@ -25,6 +26,8 @@ def identify_website(search_link):
         current_website = THEPROTOCOL
     elif "rocketjobs" in search_link:
         current_website = ROCKETJOBS
+    elif "justjoin" in search_link:
+        current_website = JUSTJOINIT
     else:
         print("identify_website: Error: Website not recognized")
         current_website = "Error: Website not recognized"
