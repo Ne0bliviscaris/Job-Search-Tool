3861d66ccbc1624e3161f6562e26e5b8899c7f48
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-09-19 13:37:00 +0200

    simplified sync

--- modules/sync/sync.py
+++ modules/sync/sync.py
@@ -25,30 +25,27 @@ def sync_records():
     Main function to oversee the synchronisation process
     Extract records from raw, add additional information and return processed data into a new file
     """
-
     raw = load_csv(RAW_FILE)
     current_file = load_csv(SYNCED_FILE)
-    new_records = []
-    current_ids = set(current_file["id"]) if not current_file.empty else []
+    current_ids = set(current_file["id"]) if not current_file.empty else set()
     raw_ids = set(raw["id"])
 
     # Archive missing records
-    missing_ids = list(current_ids - raw_ids) if current_ids else []
+    missing_ids = current_ids - raw_ids
     if missing_ids:
         cleaned_current_file = archive_records(current_file, missing_ids)
     else:
         cleaned_current_file = current_file
 
-    # If the record is new, mark it and add a timestamp
-    for _, record in raw.iterrows():
-        if record["id"] not in current_ids:
-            record["added_date"] = datetime.now().strftime("%Y-%m-%d %H:%M")
-            new_records.append(record)
+    # # If the record is new, mark it and add a timestamp
+    new_ids = raw_ids - current_ids
+    new_records = raw[raw["id"].isin(new_ids)]
+    if not new_records.empty:
+        new_records = add_timestamp(new_records, "added_date")
 
     # If there are new records, append them to the synced DataFrame
-    if new_records:
-        new_records_df = pd.DataFrame(new_records)
-        synced_df = pd.concat([cleaned_current_file, new_records_df], ignore_index=True)
+    if not new_records.empty:
+        synced_df = pd.concat([cleaned_current_file, new_records], ignore_index=True)
     else:
         synced_df = cleaned_current_file
 
@@ -63,13 +60,11 @@ def archive_records(synced, missing_ids):
     records_to_archive = synced[synced["id"].isin(missing_ids)]
 
     if not records_to_archive.empty:
-        # Add Archived_date column
-        records_to_archive["Archived_date"] = datetime.now().strftime("%Y-%m-%d %H:%M")
-
-        # Check if the archive file is empty or doesn't exist
-        is_empty_archive = not os.path.exists(ARCHIVE_FILE) or os.stat(ARCHIVE_FILE).st_size == 0
+        # Add archived_date column
+        records_to_archive = add_timestamp(records_to_archive, "archived_date")
 
         # Append records to the archive file, adding header if the file is empty
+        is_empty_archive = not os.path.exists(ARCHIVE_FILE) or os.stat(ARCHIVE_FILE).st_size == 0
         records_to_archive.to_csv(ARCHIVE_FILE, mode="a", header=is_empty_archive, index=False)
 
         # Remove archived records from synced
@@ -92,3 +87,11 @@ def show_archive():
     """
     archived = load_csv(ARCHIVE_FILE)
     return pd.DataFrame(archived) if not archived.empty else pd.DataFrame()
+
+
+def add_timestamp(records_frame, column_name):
+    """
+    Add a timestamp to the records DataFrame
+    """
+    records_frame[column_name] = datetime.now().strftime("%Y-%m-%d %H:%M")
+    return records_frame
