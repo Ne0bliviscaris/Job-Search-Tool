8d14188a4635933a9d828b4d7cd677893eac155e
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-09-19 16:07:54 +0200

    readability improvment

--- modules/sync/sync.py
+++ modules/sync/sync.py
@@ -26,9 +26,13 @@ def set_columns(file):
     return set(file[COLUMNS_TO_COMPARE].apply(tuple, axis=1))
 
 
-def filter_records_by_columns(df, records):
-    """Filter DataFrame by records based on COLUMNS_TO_COMPARE."""
-    return df[df[COLUMNS_TO_COMPARE].apply(tuple, axis=1).isin(records)]
+def filter_records(df, records):
+    """Returns only df records matching given records.
+    Compares content of columns specified in COLUMNS_TO_COMPARE."""
+    columns = df[COLUMNS_TO_COMPARE]
+    df_rows = columns.apply(tuple, axis=1)
+    is_in_records = df_rows.isin(records)
+    return df[is_in_records]
 
 
 def sync_records():
@@ -41,8 +45,8 @@ def sync_records():
     # Archive missing records
     cleaned_current_file = archive_records(current_file, missing_records)
 
-    # If the record is new, mark it and add a timestamp
-    new_records_df = filter_records_by_columns(update, new_records)
+    # If the record is new, add a timestamp
+    new_records_df = filter_records(update, new_records)
     if not new_records_df.empty:
         new_records_df = add_timestamp(new_records_df, "added_date")
         cleaned_current_file = pd.concat([cleaned_current_file, new_records_df], ignore_index=True)
@@ -55,7 +59,7 @@ def archive_records(current_file, missing_records):
     """
     Archive missing records from synced file
     """
-    records_to_archive = filter_records_by_columns(current_file, missing_records)
+    records_to_archive = filter_records(current_file, missing_records)
 
     if not records_to_archive.empty:
         records_to_archive = add_timestamp(records_to_archive, "archived_date")
@@ -65,9 +69,19 @@ def archive_records(current_file, missing_records):
         save_csv(records_to_archive, ARCHIVE_FILE, mode="a", header=is_empty_archive)
 
         # Remove archived records from current_file
-        current_file = current_file[~current_file[COLUMNS_TO_COMPARE].apply(tuple, axis=1).isin(missing_records)]
+        current_file = remove_archived_records(current_file, missing_records)
 
     return current_file
+
+
+def remove_archived_records(df, records_to_remove):
+    """
+    Remove records from DataFrame based on compared columns content.
+    """
+    columns = df[COLUMNS_TO_COMPARE]
+    rows_as_tuples = columns.apply(tuple, axis=1)
+    not_in_records_to_remove = ~rows_as_tuples.isin(records_to_remove)
+    return df[not_in_records_to_remove]
 
 
 def changed_records():
@@ -83,7 +97,7 @@ def changed_records():
     return missing_records, new_records, current_file, update
 
 
-def show_records(file):
+def show_records(file) -> pd.DataFrame:
     """Load and display records from a CSV file."""
     return load_csv(file)
 
