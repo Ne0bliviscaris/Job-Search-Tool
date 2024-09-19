09ac0493ced51c1b5e850018e78a662d553fdcf7
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-09-19 15:42:26 +0200

    readability improvements

--- modules/sync/sync.py
+++ modules/sync/sync.py
@@ -26,23 +26,23 @@ def set_columns(file):
     return set(file[COLUMNS_TO_COMPARE].apply(tuple, axis=1))
 
 
+def filter_records_by_columns(df, records):
+    """Filter DataFrame by records based on COLUMNS_TO_COMPARE."""
+    return df[df[COLUMNS_TO_COMPARE].apply(tuple, axis=1).isin(records)]
+
+
 def sync_records():
     """
     Main function to oversee the synchronisation process
     Extract records from raw, add additional information and return processed data into a new file
     """
-    raw = load_csv(RAW_FILE)
-    current_file = load_csv(SYNCED_FILE)
-    current_records = set_columns(current_file) if not current_file.empty else set()
-    raw_records = set_columns(raw)
+    missing_records, new_records, current_file, update = changed_records()
 
     # Archive missing records
-    missing_records = current_records - raw_records
-    cleaned_current_file = archive_records(current_file, missing_records) if missing_records else current_file
+    cleaned_current_file = archive_records(current_file, missing_records)
 
-    # # If the record is new, mark it and add a timestamp
-    new_records = raw_records - current_records
-    new_records_df = raw[raw[COLUMNS_TO_COMPARE].apply(tuple, axis=1).isin(new_records)]
+    # If the record is new, mark it and add a timestamp
+    new_records_df = filter_records_by_columns(update, new_records)
     if not new_records_df.empty:
         new_records_df = add_timestamp(new_records_df, "added_date")
         cleaned_current_file = pd.concat([cleaned_current_file, new_records_df], ignore_index=True)
@@ -51,11 +51,11 @@ def sync_records():
     cleaned_current_file.to_csv(SYNCED_FILE, index=False)
 
 
-def archive_records(synced, missing_records):
+def archive_records(current_file, missing_records):
     """
     Archive missing records from synced file
     """
-    records_to_archive = synced[synced[COLUMNS_TO_COMPARE].apply(tuple, axis=1).isin(missing_records)]
+    records_to_archive = filter_records_by_columns(current_file, missing_records)
 
     if not records_to_archive.empty:
         records_to_archive = add_timestamp(records_to_archive, "archived_date")
@@ -64,10 +64,23 @@ def archive_records(synced, missing_records):
         is_empty_archive = not os.path.exists(ARCHIVE_FILE) or os.stat(ARCHIVE_FILE).st_size == 0
         save_csv(records_to_archive, ARCHIVE_FILE, mode="a", header=is_empty_archive)
 
-        # Remove archived records from synced
-        synced = synced[~synced[COLUMNS_TO_COMPARE].apply(tuple, axis=1).isin(missing_records)]
+        # Remove archived records from current_file
+        current_file = current_file[~current_file[COLUMNS_TO_COMPARE].apply(tuple, axis=1).isin(missing_records)]
 
-    return synced
+    return current_file
+
+
+def changed_records():
+    """Load files and return missing and new records."""
+    update = load_csv(RAW_FILE)
+    current_file = load_csv(SYNCED_FILE)
+    current_records = set_columns(current_file) if not current_file.empty else set()
+    update_records = set_columns(update)
+
+    missing_records = current_records - update_records
+    new_records = update_records - current_records
+
+    return missing_records, new_records, current_file, update
 
 
 def show_records(file):
