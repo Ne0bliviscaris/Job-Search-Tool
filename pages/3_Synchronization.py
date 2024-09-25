e7d097ed14f07c8fb04a129797df1dc0cd348170
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-09-25 17:36:21 +0200

    clean up

--- pages/3_Synchronization.py
+++ pages/3_Synchronization.py
@@ -9,42 +9,6 @@ column_config = set_column_config()
 
 
 st.title("Synchronization module")
-with st.expander("Plan: Create a synchronization module"):
-    st.markdown(
-        """
-            - Extract data from records.csv and process them into a new file
-            """
-    )
-with st.expander("Idea"):
-    st.markdown(
-        """
-            - Sync function that will oversee the process:
-                - Create a class that will handle the data processing
-                - Function to move closed offers to the archive and add the date of closure
-            """
-    )
-with st.expander("Class object to handle the data processing"):
-    st.markdown(
-        """
-            - Unify simillar job tags, split them into categories easy to use in filters
-            - Enrich the data with additional information
-                - Date of creation
-                - Single press [applied] button to add current date
-                - Application date
-                - Feedback received
-                - Notes
-                - Add personal rating (1-5 stars)
-
-                - **Add application button (idea)**
-                - **Scrape the job offer content (idea)**
-            - Return the processed data as a dataframe with editable fields:
-                - Application date
-                - Feedback received
-                - Notes
-                - Personal rating
-            """
-    )
-
 st.write("---")
 if st.button("Synchronize Records"):
     archived_records, new_records = sync_records()
