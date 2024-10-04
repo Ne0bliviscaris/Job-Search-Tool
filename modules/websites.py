d7bfe7caeba0ae107170a84477c1729c6b5baf75
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-10-04 15:35:29 +0200

    improved links

--- modules/websites.py
+++ modules/websites.py
@@ -10,15 +10,18 @@ SOLIDJOBS = "https://solid.jobs"
 
 search_links = {
     # # Remote offers
-    "nofluffjobs_data-ai-trainee-junior-remote": "https://nofluffjobs.com/pl/praca-zdalna/artificial-intelligence?criteria=category%3Ddata%20seniority%3Dtrainee,junior",
-    "pracujpl_data-ai-junior-remote": "https://it.pracuj.pl/praca/praca%20zdalna;wm,home-office?et=17&ap=true&its=ai-ml%2Cbig-data-science",
-    "pracujpl_data-ai-junior-remote": "https://it.pracuj.pl/praca/praca%20zdalna;wm,home-office?et=17&ap=true&its=ai-ml%2Cbig-data-science",
-    "bulldogjob_data-junior-remote": "https://bulldogjob.pl/companies/jobs/s/role,data/city,Remote/experienceLevel,junior,intern",
-    "theprotocol_python-big-data-ai-ml-junior-assistant-trainee-remote": "https://theprotocol.it/filtry/python;t/big-data-science,ai-ml;sp/junior,assistant,trainee;p/zdalna;rw",
+    # Junior offers
+    "nofluffjobs_data-python-trainee-junior-remote": "https://nofluffjobs.com/pl/praca-zdalna/data?criteria=requirement%3DPython%20%20seniority%3Dtrainee,junior",
+    "nofluffjobs_ai-python-trainee-junior-remote": "https://nofluffjobs.com/pl/praca-zdalna/artificial-intelligence?criteria=requirement%3DPython%20%20seniority%3Dtrainee,junior",
+    "pracujpl_data-ai-python-junior-remote": "https://it.pracuj.pl/praca/praca%20zdalna;wm,home-office?et=17&ap=true&its=ai-ml%2Cbig-data-science&itth=37",
+    "bulldogjob_data-junior-python-remote": "https://bulldogjob.pl/companies/jobs/s/city,Remote/experienceLevel,junior,intern/skills,Python/role,data",
+    "theprotocol_python-data-ai-python-junior-remote": "https://theprotocol.it/filtry/python;t/big-data-science,ai-ml;sp/junior,assistant,trainee;p/zdalna;rw",
     "rocketjobs_data-junior-remote": "https://rocketjobs.pl/wszystkie-lokalizacje/bi-data/doswiadczenie_staz-junior/praca-zdalna_tak",
-    "justjoinit_data-junior-remote": "https://justjoin.it/all-locations/data/experience-level_junior/remote_yes?orderBy=DESC&sortBy=newest",
-    "justjoinit_ai-junior-remote": "https://justjoin.it/all-locations/ai/experience-level_junior/remote_yes?orderBy=DESC&sortBy=newest",
+    "justjoinit_data-python-junior-remote": "https://justjoin.it/all-locations/data/experience-level_junior/remote_yes?keyword=python",
+    "justjoinit_ai-python-junior-remote": "https://justjoin.it/all-locations/ai/experience-level_junior/remote_yes?keyword=python",
     "solidjobs_data-junior-remote": "https://solid.jobs/offers/it;cities=Praca%20zdalna;categories=Data%20Science;experiences=Junior",
+    "solidjobs_python-junior-remote": "https://solid.jobs/offers/it;cities=Praca%20zdalna;subcategories=Python;experiences=Junior",
+    # Mid offers
     #
     # # Hybrid/local offers
     # "nofluffjobs_data-ai-trainee-junior-poznan": "https://nofluffjobs.com/pl/hybrid/artificial-intelligence?criteria=city%3Dpoznan%20category%3Ddata%20seniority%3Dtrainee,junior",
