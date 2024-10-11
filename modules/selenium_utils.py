d58517c074624a53bdfbbdb3618f22b69cb48ef8
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-10-11 12:59:33 +0200

    improved file structure

--- modules/selenium_utils.py
+++ modules/selenium_utils.py
@@ -1,25 +1,40 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 import modules.containers as containers
+import modules.site_specific_actions as site_specific_actions
 from modules.websites import PRACUJPL
 
 
-def get_container(driver: webdriver.Chrome, search_link: str) -> str:
+def scrape(web_driver, search_link: str) -> str:
+    """Scrape given link using Selenium."""
+    web_driver.get(search_link)
+    perform_additional_action(web_driver, search_link)
+    html_content = get_search_container(web_driver, search_link)
+    return html_content
+
+
+def get_search_container(driver: webdriver.Chrome, search_link: str) -> str:
     """
     Get the HTML content of the search container using Selenium
     """
     search_container = containers.search(search_link)
     if not search_container:
-        print(f"No results for: {search_link}")
+        print(f"Website not supported yet: {search_link}")
         return ""
     try:
         search_block = driver.find_element(By.CSS_SELECTOR, search_container)
         return search_block.get_attribute("outerHTML")
     except Exception as e:
-        print(f"No results for: {search_link}")
+        print(f"Search container empty or not recognized for: {search_link}")
         return ""
+
+
+def perform_additional_action(driver: webdriver.Chrome, search_link: str):
+    """Perform additional actions depending on the page."""
+    if PRACUJPL in search_link:
+        site_specific_actions.pracujpl_confirm_cookies(driver)
+        site_specific_actions.pracujpl_click_multi_location_offer(driver)
 
 
 def setup_webdriver():
@@ -29,7 +44,14 @@ def setup_webdriver():
     # Path to container with Chrome
     docker_chrome_url = "http://localhost:4444/wd/hub"
 
-    # Chrome options
+    options = set_chromedriver_options()
+
+    # Return WebDriver instance
+    return webdriver.Remote(command_executor=docker_chrome_url, options=options)
+
+
+def set_chromedriver_options():
+    """Set options for Chrome WebDriver."""
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=old")  # Run Chrome in headless mode - no window is displayed
     options.add_argument("--disable-gpu")  # Disable GPU (optional but recommended in headless mode)
@@ -40,42 +62,4 @@ def setup_webdriver():
     options.add_argument("--log-level=2")  # Hide unnecessary logs
     options.add_argument("--disable-webgl")  # Disable WebGL
     options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})  # Disable images
-
-    # Return WebDriver instance
-    return webdriver.Remote(command_executor=docker_chrome_url, options=options)
-
-
-def scrape(web_driver, search_link: str) -> str:
-    """Scrape given link using Selenium."""
-    web_driver.get(search_link)
-    perform_additional_action(web_driver, search_link)
-    html_content = get_container(web_driver, search_link)
-    return html_content
-
-
-def perform_additional_action(driver: webdriver.Chrome, search_link: str) -> None:
-    """Perform additional actions depending on the page."""
-    if PRACUJPL in search_link:
-        pracujpl_confirm_cookies(driver)
-        click_pracujpl_multiloc(driver)
-
-
-def click_pracujpl_multiloc(driver: webdriver.Chrome):
-    """Open all multilocation records to get offer link."""
-    css_selector = '[data-test-location="multiple"]'
-    try:
-        elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
-        for element in elements:
-            element.click()
-    except Exception as e:
-        pass
-
-
-def pracujpl_confirm_cookies(driver):
-    """Confirm cookies on Pracuj.pl."""
-    css_selector = '[data-test="button-submitCookie"]'
-    try:
-        element = driver.find_element(By.CSS_SELECTOR, css_selector)
-        element.click()
-    except Exception as e:
-        pass
+    return options
