9499b5711951bbe02ff38c5386013a9ff85ef8ac
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-11-15 16:53:59 +0100

    moved remote work dictionary to separate file

---
+++ modules/dicts.py
@@ -0,0 +1,14 @@
+remote_work_dict = {
+    "Remote": [
+        "remote",
+        "zdalna",
+        "zdalnie",
+        "poland (remote)",
+        "100% zdalnie",
+        "cała polska (praca zdalna)",
+        ", Fully remote",
+        "fully remotefully remote",
+    ],
+    "Hybrid": ["hybrid", "hybrydowa", "remote hybrid", "hybryd"],
+    "Stationary": ["stationary", "stacjonarna", "full office"],
+}
