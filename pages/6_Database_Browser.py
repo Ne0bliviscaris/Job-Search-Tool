34ea2ff17b8358b3ea1c09fe083eb06cc0e19878
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-11-26 17:55:51 +0100

    separated 'save records' button

--- pages/6_Database_Browser.py
+++ pages/6_Database_Browser.py
@@ -9,15 +9,22 @@ from modules.dataframe_settings import column_conversions
 st.set_page_config(layout="wide")
 
 
+def save_changes_button(changed_dataframe):
+    """Display the save changes button."""
+    if st.button("Save Changes"):
+        save_records_to_db(changed_dataframe)
+        st.success("Changes saved successfully!")
+
+
 def db_frame():
     """Main function to display the job offers browser as dataframe."""
     st.title("Job Offers Database Browser")
     raw_frame = load_records_from_db()
     if not raw_frame.empty:
         main_frame = column_conversions(raw_frame)
-        if st.button("Save Changes"):
-            save_records_to_db(main_frame)
-            st.success("Changes saved successfully!")
+        save_changes_button(main_frame)
+    else:
+        st.warning("Database empty. Perform update to find records.")
 
 
 db_frame()
