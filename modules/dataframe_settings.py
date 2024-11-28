7a347f056c38f741f5d8a7d62bd4d27dc90f2cb2
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-11-28 14:09:12 +0100

    unified date format, ensuring integer data in number columns, enforcing proper personal rating format, column conversions handle streamlit keys required fix in calculate_elapsed_days

--- modules/dataframe_settings.py
+++ modules/dataframe_settings.py
@@ -5,6 +5,7 @@ pd.set_option("future.no_silent_downcasting", True)
 
 ALL_COLUMNS = [
     # From JobRecords class
+    "id",
     "title",
     "logo",
     "company_name",
@@ -33,6 +34,7 @@ ALL_COLUMNS = [
 
 MAIN_FRAME_COLUMNS = [
     # From JobRecords class
+    "id",
     "title",
     "logo",
     "company_name",
@@ -56,9 +58,12 @@ MAIN_FRAME_COLUMNS = [
     "feedback_date",
     "time_until_feedback",
     "offer_status",
+    "archived_date",
+    "users_id",
 ]
 
 ARCHIVE_COLUMNS = [
+    "id",
     "title",
     "logo",
     "company_name",
@@ -88,6 +93,7 @@ def set_column_config(archive=False):
     application_statuses = ["Not applied", "Applied", "Interview", "Hired"]
 
     static_columns = {
+        "id": st.column_config.TextColumn("ID", disabled=True),
         "title": st.column_config.TextColumn("Title", disabled=True),
         "company_name": st.column_config.TextColumn("Company Name", disabled=True),
         "location": st.column_config.TextColumn("Location", disabled=True),
@@ -96,33 +102,37 @@ def set_column_config(archive=False):
         "salary_text": st.column_config.TextColumn("Salary Text", disabled=True),
         "tags": st.column_config.TextColumn("Tags", disabled=True),
         "logo": st.column_config.ImageColumn("Logo", width=100),
-        "min_salary": st.column_config.NumberColumn("Min Salary", disabled=True),
-        "max_salary": st.column_config.NumberColumn("Max Salary", disabled=True),
-        "elapsed_days": st.column_config.NumberColumn("Elapsed Days", disabled=True),
+        "min_salary": st.column_config.NumberColumn("Min Salary", disabled=True, format="%d"),
+        "max_salary": st.column_config.NumberColumn("Max Salary", disabled=True, format="%d"),
+        "elapsed_days": st.column_config.NumberColumn("Elapsed Days", disabled=True, format="%d"),
         "website": st.column_config.LinkColumn("Website", disabled=True),
         "added_date": date_column("Added date"),
         "archived_date": date_column("Archived date"),
     }
 
     conditionally_editable_columns = {
-        "application_status": st.column_config.SelectboxColumn(
-            "Application Status", options=application_statuses, default="Not applied", disabled=archive
-        ),
-        "notes": st.column_config.TextColumn("Notes", disabled=archive),
-        "personal_rating": st.column_config.NumberColumn("Personal Rating", disabled=False),
         "time_until_feedback": st.column_config.NumberColumn("Time Until Feedback", disabled=archive),
-        "feedback_received": st.column_config.CheckboxColumn("Feedback received", disabled=archive),
+        "url": st.column_config.LinkColumn("URL", width=100, disabled=archive),
+    }
+    editable_columns = {
+        "application_status": st.column_config.SelectboxColumn(
+            "Application Status", options=application_statuses, default="Not applied", disabled=False
+        ),
+        "notes": st.column_config.TextColumn("Notes", disabled=False),
+        "personal_rating": st.column_config.NumberColumn(
+            "Personal Rating", disabled=False, format="%d", min_value=0, max_value=5, step=1
+        ),
+        "feedback_received": st.column_config.CheckboxColumn("Feedback received", disabled=False),
         "application_date": date_column("Application date"),
         "feedback_date": date_column("Feedback date"),
-        "url": st.column_config.LinkColumn("URL", width=100, disabled=archive),
     }
 
     # Combine both dictionaries
-    column_config = {**static_columns, **conditionally_editable_columns}
+    column_config = {**static_columns, **conditionally_editable_columns, **editable_columns}
     return column_config
 
 
-def column_conversions(frame, archive=False):
+def column_conversions(frame, archive=False, key=None):
     """Return column conversions for the data editor."""
     columns = MAIN_FRAME_COLUMNS if not archive else ARCHIVE_COLUMNS
     frame = add_missing_columns(frame)
@@ -132,7 +142,7 @@ def column_conversions(frame, archive=False):
     frame = check_application_status(frame)
     frame = check_feedback_status(frame)
 
-    edited_df = st.data_editor(frame[columns], disabled=archive, column_config=set_column_config(archive))
+    edited_df = st.data_editor(frame[columns], disabled=archive, column_config=set_column_config(archive), key=key)
     return edited_df
 
 
@@ -141,8 +151,9 @@ def convert_date_columns(frame):
     date_columns = ["added_date", "application_date", "feedback_date", "archived_date"]
     for col in date_columns:
         if col not in frame.columns:
-            frame[col] = pd.NaT
-        frame[col] = pd.to_datetime(frame[col], errors="coerce")
+            # frame[col] = pd.NaT
+            frame[col] = None
+        frame[col] = pd.to_datetime(frame[col], errors="coerce", format="%d-%m-%Y").dt.date
     return frame
 
 
@@ -150,7 +161,7 @@ def add_missing_columns(frame):
     """Add missing columns to the DataFrame."""
     missing_columns = set(ALL_COLUMNS) - set(frame.columns)
     for col in missing_columns:
-        frame[col] = pd.NA
+        frame[col] = None
     return frame
 
 
@@ -162,26 +173,31 @@ def fill_missing_values(frame):
     return frame
 
 
+from datetime import datetime
+
+
 def calculate_elapsed_days(frame, archive=False):
     """Calculate elapsed days since the job offer was added."""
-    now = pd.Timestamp.now()
+    now = datetime.now().date()
     # Calculate elapsed days
 
-    added_date = frame["added_date"]
-    archived_date = frame["archived_date"]
-    application_date = frame["application_date"]
-    feedback_date = frame["feedback_date"]
+    # Fix needed
+    added_date = frame["added_date"] if frame["added_date"].notna().all() else None
+    archived_date = frame["archived_date"] if frame["archived_date"].notna().all() else None
+    application_date = frame["application_date"] if frame["application_date"].notna().all() else None
+    feedback_date = frame["feedback_date"] if frame["feedback_date"].notna().all() else None
 
     if not archive:
-        frame["elapsed_days"] = (now - added_date).dt.days
+        frame["elapsed_days"] = (now - added_date).apply(lambda x: x.days if pd.notnull(x) else None)
     else:
-        frame["elapsed_days"] = (archived_date - added_date).dt.days
+        frame["elapsed_days"] = (archived_date - added_date).apply(lambda x: x.days if pd.notnull(x) else None)
 
     # Calculate time until feedback
-    if application_date is None:
-        frame["time_until_feedback"] = pd.NA
+    if application_date is not None and feedback_date is not None:
+        frame["time_until_feedback"] = (feedback_date - application_date).apply(lambda x: x.days)
     else:
-        frame["time_until_feedback"] = (feedback_date - application_date).dt.days
+        print("Application date or feedback date is missing.")
+        frame["time_until_feedback"] = None
     return frame
 
 
