7a6b43fbf4639c179159f464cd248c1e24991a6b
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-11-28 14:13:44 +0100

    preview session_state

--- pages/6_Database_Browser.py
+++ pages/6_Database_Browser.py
@@ -3,8 +3,8 @@ import os
 import streamlit as st
 
 from modules.data_processor import load_records_from_db
-from modules.database.database import save_records_to_db
-from modules.dataframe_settings import column_conversions
+from modules.database.database import update_records_in_db
+from modules.dataframe_settings import column_conversions, convert_date_columns
 
 st.set_page_config(layout="wide")
 
@@ -12,7 +12,8 @@ st.set_page_config(layout="wide")
 def save_changes_button(changed_dataframe):
     """Display the save changes button."""
     if st.button("Save Changes"):
-        save_records_to_db(changed_dataframe)
+        column_conversions(changed_dataframe)
+        update_records_in_db(changed_dataframe)
         st.success("Changes saved successfully!")
 
 
@@ -21,10 +22,12 @@ def db_frame():
     st.title("Job Offers Database Browser")
     raw_frame = load_records_from_db()
     if not raw_frame.empty:
-        main_frame = column_conversions(raw_frame)
+        main_frame = column_conversions(raw_frame, key="main_frame")
+        # main_frame = st.data_editor(raw_frame, key="main_frame")
         save_changes_button(main_frame)
     else:
         st.warning("Database empty. Perform update to find records.")
 
 
 db_frame()
+st.session_state
