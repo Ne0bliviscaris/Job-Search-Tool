ee913a3a81729a50510e3849a6cb294babbf6df0
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-11-28 15:06:45 +0100

    temporary solution - column conversions to set up date acceptable for sqlite

--- pages/6_Database_Browser.py
+++ pages/6_Database_Browser.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 
 from modules.data_processor import load_records_from_db
-from modules.database.database import update_records_in_db
+from modules.database.database import update_edited_dataframe, update_record
 from modules.dataframe_settings import column_conversions, convert_date_columns
 
 st.set_page_config(layout="wide")
@@ -13,7 +13,8 @@ def save_changes_button(changed_dataframe):
     """Display the save changes button."""
     if st.button("Save Changes"):
         column_conversions(changed_dataframe)
-        update_records_in_db(changed_dataframe)
+        update_edited_dataframe(changed_dataframe, st.session_state)
+
         st.success("Changes saved successfully!")
 
 
@@ -22,7 +23,7 @@ def db_frame():
     st.title("Job Offers Database Browser")
     raw_frame = load_records_from_db()
     if not raw_frame.empty:
-        main_frame = column_conversions(raw_frame, key="main_frame")
+        main_frame = column_conversions(raw_frame, key="editable_dataframe")
         # main_frame = st.data_editor(raw_frame, key="main_frame")
         save_changes_button(main_frame)
     else:
--- pages/7_Archive_DB_Browser.py
+++ pages/7_Archive_DB_Browser.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 
 from modules.data_processor import load_records_from_db
-from modules.database.database import update_records_in_db
+from modules.database.database import update_edited_dataframe
 from modules.dataframe_settings import column_conversions
 
 st.set_page_config(layout="wide")
@@ -14,9 +14,10 @@ def db_frame():
     st.title("Job Offers Database Browser")
     raw_frame = load_records_from_db(archive=True)
     if not raw_frame.empty:
-        archive_frame = column_conversions(raw_frame, "Archive")
+        archive_frame = column_conversions(raw_frame, archive="Archive", key="editable_dataframe")
         if st.button("Save Changes"):
-            update_records_in_db(archive_frame)
+            archive_frame = column_conversions(archive_frame, archive="Archive")
+            update_edited_dataframe(archive_frame, st.session_state)
             st.success("Changes saved successfully!")
 
     else:
