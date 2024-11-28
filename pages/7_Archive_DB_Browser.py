cbb5295d211adb960a9e4499b7b04c3082b0b6cd
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-11-28 14:12:07 +0100

    required fix to saving changes

--- pages/7_Archive_DB_Browser.py
+++ pages/7_Archive_DB_Browser.py
@@ -2,9 +2,8 @@ import os
 
 import streamlit as st
 
-import modules.database.database as data_processor
 from modules.data_processor import load_records_from_db
-from modules.database.database import save_records_to_db
+from modules.database.database import update_records_in_db
 from modules.dataframe_settings import column_conversions
 
 st.set_page_config(layout="wide")
@@ -15,14 +14,11 @@ def db_frame():
     st.title("Job Offers Database Browser")
     raw_frame = load_records_from_db(archive=True)
     if not raw_frame.empty:
-        main_frame = column_conversions(raw_frame, "Archive")
+        archive_frame = column_conversions(raw_frame, "Archive")
         if st.button("Save Changes"):
-            save_records_to_db(main_frame)
+            update_records_in_db(archive_frame)
             st.success("Changes saved successfully!")
 
-        if st.button("Save modifications"):
-            data_processor.save_records_to_db(raw_frame)
-            st.success("Saved to database!")
     else:
         st.warning("No records found.")
 
