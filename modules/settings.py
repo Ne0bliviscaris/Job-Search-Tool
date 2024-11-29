02754e158a368ba96f91f79cab171bc17d36b0e3
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2024-11-29 13:55:55 +0100

    added chromedriver path to settings along with instructions to launch it

--- modules/settings.py
+++ modules/settings.py
@@ -1,4 +1,10 @@
 DATE_FORMAT = "%Y-%m-%d"
 
+# Install the following Docker container to run standalone chromedriver
+# https://hub.docker.com/r/selenium/standalone-chrome
+# Make sure you replace the IP address with the one used by your Docker container
+CHROMEDRIVER_CONTAINER = "http://localhost:4444/wd/hub"
+
+# Debug mode switch
 DEBUG_MODE = False
 # DEBUG_MODE = True
