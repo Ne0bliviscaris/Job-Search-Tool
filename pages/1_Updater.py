39e074c47feba2c1ce963e1c849dbe25b4c312f5
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-02-06 13:36:16 +0100

    import fix for separated updater from progress bar

--- pages/1_Updater.py
+++ pages/1_Updater.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from modules.updater.updater import update_all_sites
+from modules.updater.updater import update_sites_with_progress_bar
 
 st.title("Update all sites")
 st.write("This script will update all sites by downloading the latest HTML content.")
@@ -9,4 +9,4 @@ st.write("Please wait until the process is finished.")
 if st.button("Update All Sites"):
     """Update all sites data when button is clicked"""
     with st.spinner("Updating..."):  # Display a spinner while updating
-        update_all_sites(st)
+        update_sites_with_progress_bar(st)
