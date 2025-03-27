9f1bd6eacd90bd1ea5a866e48cd230c02d0fb2cd
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-03-27 14:39:31 +0100

    test fixed

--- modules/tests/sync/test_process_new_records.py
+++ modules/tests/sync/test_process_new_records.py
@@ -31,7 +31,7 @@ def current_db():
     return get_dummy_db_records()
 
 
-@patch("modules.sync.sync.save_records_to_db")
+@patch("modules.updater.data_processing.sync.save_records_to_db")
 def test_empty_update(mock_save_records_to_db, empty_new_records):
     """Test process_new_records with empty DataFrames."""
     process_new_records(empty_new_records)
@@ -40,7 +40,7 @@ def test_empty_update(mock_save_records_to_db, empty_new_records):
     mock_save_records_to_db.assert_not_called()
 
 
-@patch("modules.sync.sync.save_records_to_db")
+@patch("modules.updater.data_processing.sync.save_records_to_db")
 def test_new_records(mock_save_records_to_db, new_records):
     """Test process_new_records with new_records containing two records."""
     process_new_records(new_records)
@@ -49,7 +49,13 @@ def test_new_records(mock_save_records_to_db, new_records):
     mock_save_records_to_db.assert_called_once()
     saved_df = mock_save_records_to_db.call_args[0][0]
     assert len(saved_df) == 5  # All 5 records should be saved
-    assert all(saved_df["title"] == ["Job1", "Job2", "Job8", "Job9", "Job10"])
-    assert all(saved_df["company_name"] == ["Company1", "Company2", "Company8", "Company9", "Company10"])
+
+    expected_titles = pd.Series(["Job1", "Job2", "Job8", "Job9", "Job10"])
+    expected_companies = pd.Series(["Company1", "Company2", "Company8", "Company9", "Company10"])
+
+    assert saved_df["title"].reset_index(drop=True).equals(expected_titles)
+    assert saved_df["company_name"].reset_index(drop=True).equals(expected_companies)
     assert "added_date" in saved_df.columns
-    assert all(saved_df["added_date"] == pd.to_datetime(datetime.now().strftime(DATE_FORMAT)))
+
+    today = pd.to_datetime(datetime.now().strftime(DATE_FORMAT))
+    assert all(saved_df["added_date"].dt.strftime(DATE_FORMAT) == today.strftime(DATE_FORMAT))
