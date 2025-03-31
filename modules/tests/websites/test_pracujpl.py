94774a03d184305da2ccdbaa2f7aea65d44d6b78
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-03-31 21:07:19 +0200

    test: add unit tests for PracujPL scraper functionality

---
+++ modules/tests/websites/test_pracujpl.py
@@ -0,0 +1,46 @@
+import pytest
+from bs4 import BeautifulSoup
+
+from modules.updater.scraper.selenium_utils import setup_webdriver
+from modules.updater.sites.websites.PracujPL import PracujPL
+
+empty_listing = "https://it.pracuj.pl/praca?et=1&sal=50000"
+job_listing = "https://it.pracuj.pl/praca"
+
+
+def test_search_container():
+    """Test the search functionality of Pracuj.pl."""
+    # Initialize the PracujPL class with an empty search link
+    with setup_webdriver() as web_driver:
+        site = PracujPL(search_link=job_listing)
+        search_container = site.scrape(web_driver)
+        assert search_container is not None, "Search container is None"
+
+
+def test_scrape_empty_container():
+    """Test the search functionality of Pracuj.pl."""
+    # Initialize the PracujPL class with an empty search link
+    with setup_webdriver() as web_driver:
+        site = PracujPL(search_link=empty_listing)
+        empty_search_container = site.scrape(web_driver)
+        assert "No offers found" in empty_search_container, "Search container is not empty when it should be"
+
+
+def test_records_list():
+    """Test job records extraction from HTML."""
+    with setup_webdriver() as web_driver:
+
+        site = PracujPL(search_link=job_listing)
+        search_container = site.scrape(web_driver)
+        soup = BeautifulSoup(search_container, "html.parser")
+        records = site.records_list(html=soup)
+        assert len(records) > 0, "No records found in the search container"
+
+
+def test_empty_records_list():
+    """Test the records list extraction."""
+    with setup_webdriver() as web_driver:
+        site = PracujPL(search_link=empty_listing)
+        search_container = site.scrape(web_driver)
+        records = site.records_list(html=search_container)
+        assert records is None or len(records) == 0, "Records found in empty search container"
