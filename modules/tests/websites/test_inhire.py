920ca0c40450590d97ae26ba3f9bbc9cf3596b95
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-04-30 15:06:46 +0200

    test: added basic unit tests for InhireIO

--- modules/tests/websites/test_inhire.py
+++ modules/tests/websites/test_inhire.py
@@ -1,43 +1,43 @@
 from bs4 import BeautifulSoup
 
 from modules.updater.sites.websites.InhireIO import InhireIO
+from modules.updater.updater import update_site
 from modules.updater.webdriver import setup_webdriver
 
 empty_listing = "https://inhire.io/oferty-pracy?experiences=0_1&locations=1&roles=it,blockchain_engineer&salary=75000&technologies=413"
-job_listing = "https://solid.jobs/offers/it"
+job_listing = "https://inhire.io/?roles=it"
 
 
 def test_search_container():
     """Test the search functionality of Pracuj.pl."""
     # Initialize the PracujPL class with an empty search link
-    with setup_webdriver() as web_driver:
-        site = InhireIO(search_link=job_listing)
-        search_container = site.scrape(web_driver)
-        assert search_container is not None, "Search container is None"
+
+    site = InhireIO(search_link=job_listing)
+    search_container = site.scrape()
+    assert search_container is not None, "Search container is None"
+    assert len(search_container) > 0, "Search container is empty"
 
 
 def test_scrape_empty_container():
     """Test empty search results handling for RocketJobs."""
-    with setup_webdriver() as web_driver:
-        site = InhireIO(search_link=empty_listing)
-        empty_search_container = site.scrape(web_driver)
-        soup_empty_search = BeautifulSoup(empty_search_container, "html.parser")
-        assert len(soup_empty_search) == 0, "Empty search container is not empty"
+    site = InhireIO(search_link=empty_listing)
+    empty_search_container = site.scrape()
+    assert len(empty_search_container) == 0, "Empty search container is not empty"
 
 
 def test_records_list():
     """Test the records list extraction."""
-    with setup_webdriver() as web_driver:
-        site = InhireIO(search_link=job_listing)
-        records = site.scrape(web_driver)
-        assert len(records) > 0
+    site = InhireIO(search_link=job_listing)
+    search_block = site.scrape()
+    records = site.records_list(html=search_block, link=job_listing, test_mode=True)
+
+    assert len(records) > 0
 
 
 def test_empty_records_list():
     """Test the records list extraction."""
-    with setup_webdriver() as web_driver:
-        site = InhireIO(search_link=empty_listing)
-        search_container = site.scrape(web_driver)
-        soup = BeautifulSoup(search_container, "html.parser")
-        records = site.records_list(html=soup)
-        assert records is None or len(records) == 0, "Records found in empty search container"
+
+    site = InhireIO(search_link=empty_listing)
+    search_block = site.scrape()
+    records = site.records_list(html=search_block, link=empty_listing, test_mode=True)
+    assert len(records) == 0, "Records found in empty search container"
