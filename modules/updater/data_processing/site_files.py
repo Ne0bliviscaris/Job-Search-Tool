5207c16b6ede899a82d91a8a1c1f2ca2699cf5e6
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-05-08 23:31:54 +0200

    feature: add functions to save and load HTML content - implemented save_html function - added load_html_as_soup function

--- modules/updater/data_processing/site_files.py
+++ modules/updater/data_processing/site_files.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+from bs4 import BeautifulSoup
+
 from modules.settings import FILE_STORAGE_PATH
 from modules.websites import search_links
 
@@ -21,3 +23,15 @@ def load_json(filename) -> dict:
 def save_json(filename, data) -> None:
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
+
+
+def save_html(filename, data) -> None:
+    """Save HTML content to a file."""
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(data)
+
+
+def load_html_as_soup(filename: str) -> BeautifulSoup:
+    """Convert HTML file to BeautifulSoup object"""
+    with open(filename, "r", encoding="utf-8") as file:
+        return BeautifulSoup(file, "html.parser")
