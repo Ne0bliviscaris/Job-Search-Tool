69354a0825154a804eef9ac0e54de22bc4941ea6
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-05-08 23:06:27 +0200

    clean up: data_processor no longer needed

--- modules/updater/data_processing/data_processor.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-
-
-def html_to_soup(filename: str) -> BeautifulSoup:
-    """Convert HTML file to BeautifulSoup object"""
-    with open(filename, "r", encoding="utf-8") as file:
-        return BeautifulSoup(file, "html.parser")
