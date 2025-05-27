12ae1d22066e44f004666bdc4def000ab4edec96
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-05-27 19:37:08 +0200

    refactor: separated updater from streamlit functions

--- modules/updater/updater.py
+++ modules/updater/updater.py
@@ -1,5 +1,3 @@
-import os
-
 from modules.settings import SAVE_HTML
 from modules.updater.data_processing.site_files import set_filename_from_link
 from modules.updater.sites.SiteFactory import SiteFactory
@@ -15,25 +13,6 @@ def update_all_sites():
                 yield link_name
 
 
-# @fancy_error_handler
-def update_sites_with_progress_bar(st):
-    """Display progress bar while updating sites."""
-    progress_bar, status_box = handle_update_progress(st)
-    progress = 0
-    # Process each site using the core function that yields link_name
-    for link_name in update_all_sites():
-        progress += 1
-        update_status(progress_bar, status_box, progress, link_name)
-    st.success("All sites updated!")
-
-
-def handle_update_progress(st) -> tuple:
-    """Setup and return progress tracking elements."""
-    progress_bar = st.empty()
-    status_box = st.empty()
-    return progress_bar, status_box
-
-
 def update_site(webdriver, search_link) -> str:
     """Download HTML content from the search link and save it to a file."""
     job_site = SiteFactory.identify_website(search_link)
@@ -43,11 +22,3 @@ def update_site(webdriver, search_link) -> str:
         filename = set_filename_from_link(search_link, job_site.file_extension)
         job_site.save_file(filename, search_block)
     return search_block if search_block else True
-
-
-def update_status(progress_bar, status_box, progress: int, link_name: str) -> None:
-    """Update progress bar and status."""
-    total = len(search_links)
-    progress_bar.progress(progress / total)
-    status_box.success(f"Downloaded: {link_name}")
-    status_box.empty()
--- pages/1_Updater.py
+++ pages/1_Updater.py
@@ -1,12 +1,39 @@
 import streamlit as st
 
-from modules.updater.updater import update_sites_with_progress_bar
+from modules.updater.updater import update_all_sites
+from modules.websites import search_links
+
+
+def update_sites_with_progress_bar():
+    """Display progress bar while updating sites."""
+    progress_bar, status_box = handle_update_progress()
+    progress = 0
+    # Process each site using the core function that yields link_name
+    for link_name in update_all_sites():
+        progress += 1
+        update_status(progress_bar, status_box, progress, link_name)
+    st.success("All sites updated!")
+
+
+def handle_update_progress() -> tuple:
+    """Setup and return progress tracking elements."""
+    progress_bar = st.empty()
+    status_box = st.empty()
+    return progress_bar, status_box
+
+
+def update_status(progress_bar, status_box, progress: int, link_name: str) -> None:
+    """Update progress bar and status."""
+    total = len(search_links)
+    progress_bar.progress(progress / total)
+    status_box.success(f"Downloaded: {link_name}")
+    status_box.empty()
+
 
 st.title("Update all sites")
 st.write("This script will update all sites by downloading the latest HTML content.")
 st.write("Please wait until the process is finished.")
 
 if st.button("Update All Sites"):
-    """Update all sites data when button is clicked"""
     with st.spinner("Updating..."):  # Display a spinner while updating
-        update_sites_with_progress_bar(st)
+        update_sites_with_progress_bar()
