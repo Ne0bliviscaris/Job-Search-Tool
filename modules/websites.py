54576f1bf8e2f343a767a686975729a336318ae0
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-08-29 23:23:24 +0200

    websites: drop internship roles, add mid roles

--- modules/websites.py
+++ modules/websites.py
@@ -10,17 +10,16 @@ JUSTJOINIT = "https://justjoin.it"
 SOLIDJOBS = "https://solid.jobs"
 
 search_links = {
-    # # Remote offers
-    # Junior offers
-    "nofluffjobs_data-python-trainee-junior-remote": "https://nofluffjobs.com/pl/praca-zdalna/data?criteria=requirement%3DPython%20%20seniority%3Dtrainee,junior",
-    "nofluffjobs_ai-python-trainee-junior-remote": "https://nofluffjobs.com/pl/praca-zdalna/artificial-intelligence?criteria=requirement%3DPython%20%20seniority%3Dtrainee,junior",
-    "pracujpl_data-ai-python-junior-remote": "https://it.pracuj.pl/praca/praca%20zdalna;wm,home-office?et=17&ap=true&its=ai-ml%2Cbig-data-science&itth=37",
-    "bulldogjob_data-junior-python-remote": "https://bulldogjob.pl/companies/jobs/s/city,Remote/experienceLevel,junior,intern/skills,Python/role,data",
-    "theprotocol_python-data-ai-python-junior-remote": "https://theprotocol.it/filtry/python;t/big-data-science,ai-ml;sp/junior,assistant,trainee;p/zdalna;rw",
-    "rocketjobs_data-junior-remote": "https://rocketjobs.pl/oferty-pracy/wszystkie-lokalizacje/bi-data?doswiadczenie=staz-junior&praca-zdalna=tak&orderBy=DESC&sortBy=published&from=0",
-    "justjoinit_data-python-junior-remote": "https://justjoin.it/job-offers/all-locations/data?experience-level=junior&remote=yes&orderBy=DESC&sortBy=published&keyword=python&from=0",
-    "justjoinit_ai-python-junior-remote": "https://justjoin.it/job-offers/all-locations/ai?experience-level=junior&remote=yes&orderBy=DESC&sortBy=published&keyword=python&from=0",
-    "solidjobs_data-junior-remote": "https://solid.jobs/offers/it;cities=Praca%20zdalna;categories=Data%20Science;experiences=Junior",
-    "solidjobs_python-junior-remote": "https://solid.jobs/offers/it;cities=Praca%20zdalna;subcategories=Python;experiences=Junior",
-    "inhire_data-ai-python-junior-remote": "https://inhire.io/?experiences=1_2,0_1&locations=39&roles=it,big_data,etl_developer,machine_learning_engineer,data_science,backend_developer&technologies=7",
+    # Remote offers - junior, mid, python, data, ai
+    "nofluffjobs_data-python-junior-mid-remote": "https://nofluffjobs.com/pl/praca-zdalna/data?criteria=requirement%3DPython%20%20seniority%3Djunior,mid",
+    "nofluffjobs_ai-python-junior-mid-remote": "https://nofluffjobs.com/pl/praca-zdalna/artificial-intelligence?criteria=requirement%3DPython%20%20seniority%3Djunior,mid",
+    "pracujpl_data-ai-python-junior-mid-remote": "https://it.pracuj.pl/praca/praca%20zdalna;wm,home-office?et=17%2C4&ap=true&its=ai-ml%2Cbig-data-science&itth=37",
+    "bulldogjob_data-junior-mid-python-remote": "https://bulldogjob.pl/companies/jobs/s/city,Remote/experienceLevel,junior,medium/skills,Python/role,data",
+    "theprotocol_python-data-ai-python-junior-mid-remote": "https://theprotocol.it/filtry/python;t/big-data-science,ai-ml;sp/junior,mid;p/zdalna;rw",
+    "rocketjobs_data-junior-mid-remote": "https://rocketjobs.pl/oferty-pracy/praca-zdalna/bi-data?doswiadczenie=specjalista-mid,staz-junior&orderBy=DESC&sortBy=published",
+    "justjoinit_data-python-junior-mid-remote": "https://nofluffjobs.com/pl/praca-zdalna/data?criteria=requirement%3DPython%20%20seniority%3Djunior,mid",
+    "justjoinit_ai-python-junior-mid-remote": "https://justjoin.it/job-offers/remote/ai?experience-level=junior,mid&keyword=python&orderBy=DESC&sortBy=published",
+    "solidjobs_data-junior-mid-remote": "https://solid.jobs/offers/it;cities=Praca%20zdalna;categories=Data%20Science;experiences=Regular,Junior",
+    "solidjobs_python-junior-mid-remote": "https://solid.jobs/offers/it;cities=Praca%20zdalna;subcategories=Python;experiences=Regular,Junior",
+    "inhire_data-ai-python-junior-mid-remote": "https://inhire.io/?experiences=1_2,2_4&locations=1&roles=it,big_data,etl_developer,machine_learning_engineer,data_science,backend_developer&technologies=7",
 }
